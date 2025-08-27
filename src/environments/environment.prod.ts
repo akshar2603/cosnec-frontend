eb1af3682be7c8ec6fb5018affd049d97bbb2779
Akshar Lakhankiya <aksharlakhankiya@Akshars-Mac-mini.local>
2025-08-28 00:11:21 +0530

    google feature

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // environment.prod.ts
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://cosmos-bnqi.onrender.com',
+  apiBaseUrl: 'https://cosnec-backend.onrender.com',
   googleClientId: '662097179685-1jmjju6e9rtm8aseg121ltjgo20ncebe.apps.googleusercontent.com'
 };
